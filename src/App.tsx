2c8ab92493177ab631fb8b57726a930eaea61341
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2025-11-15 02:01:51 +0100

    Base Login and SignUp pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
-import Navbar from './components/Navbar'
 import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   )
